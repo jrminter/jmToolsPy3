342122e716f57c04ebdf7716d0d77b2bcac2ffc5
John Minter <jrminter@gmail.com>
2016-03-05 00:15:19 -0500

    added mapt tools and plotting funcs. v0.0.959.dev0

--- jmToolsPy3/__init__.py
+++ jmToolsPy3/__init__.py
@@ -1,3 +1,4 @@
+from .map_tools import *
 from .plotting_tools import *
 from .peak_detect import *
 from .savitzky_golay import *
---
+++ jmToolsPy3/map_tools.py
@@ -0,0 +1,61 @@
+"""
+map_tools: Convenience functions for processing X-Ray EDS maps
+------------------------------------------------------------------------
+    
+ Ver       Date      Who  Comments
+-------  ----------  ---  ------------------------------------------
+0.0.959  2016-03-04  JRM  Add ensureDir and fix_gray_image_to_rgb utils
+"""
+# -*- coding: utf-8 -*-
+
+def readMapFilesTif(lNames, path='./tif/', bVerbose=False):
+    """readMapFilesTif(lNames, path='./tif/', bVerbose=False)
+    
+    Read and scale a list of EDS maps with the ROI as the last element.
+    Convert to float images, scaling to the such that the maximum counts
+    in the most intense map has a gray value of 1.0
+    
+    """
+    import numpy as np
+    from skimage.external.tifffile import imread
+    imgs = []
+    maxCts = []
+    for name in lNames:
+        fPath = path + name + '.tif'
+        if bVerbose:
+            print(fPath)
+        img = imread(fPath)
+        img = img.astype(float)
+        maxCts.append(np.max(img))
+        imgs.append(img)
+    l = len(imgs)
+    if bVerbose:
+        print(l)
+        print(maxCts)
+    maxG = np.max(maxCts[0:l-1])
+    roiG = maxCts[l-1]
+    if bVerbose:
+        print(maxG, roiG)
+    for i in range(l-1):
+        imgs[i] /= maxG
+    imgs[l-1] /= roiG
+
+    return imgs, maxCts
+
+
+def denoiseMaps(imgs, wt = 0.05, ep = 0.02, cycles = 200):
+    """denoiseMaps(imgs, wt = 0.05, ep = 0.02, cycles = 200)
+    
+    A wrapper for denoise_tv_chambolle
+    
+    returns
+    
+    A list of denoised images
+    """
+    from skimage.restoration import denoise_tv_chambolle
+    denoised = []
+    for img in imgs:
+        dn = denoise_tv_chambolle(img, weight=wt, eps=ep, n_iter_max=cycles, multichannel=False)
+        denoised.append(dn)
+
+    return denoised
